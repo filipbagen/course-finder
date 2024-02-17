35c0b4f32390c478150206456638b7c4c09a618d
Filip Malm-Bägén <Filip.bagen@gmail.com>
2024-02-17 18:11:05 +0100

    finsihed filter component

--- src/pages/Home/Dashboard.tsx
+++ src/pages/Home/Dashboard.tsx
@@ -23,6 +23,7 @@ import {
 import { toast } from 'sonner';
 
 interface Course {
+  [x: string]: any;
   courseName: string;
   credits: number;
   courseCode: string;
@@ -30,20 +31,33 @@ interface Course {
   semester: number[];
   period: number[];
   block: number[];
+  courseLevel: string;
+  mainFieldOfStudy: string[];
+  studyPace: string;
   // Add other course properties as needed
 }
 
 interface SelectedFilters {
   period: number[];
   semester: number[];
-  // Add other filter types as needed, for example:
-  // block: number[];
+  block: number[];
+  courseLevel: string;
+  mainFieldOfStudy: string[];
+  location: string;
+  studyPace: string;
+  examination: string;
 }
 
 const Dashboard = () => {
   const [selectedFilters, setSelectedFilters] = useState<SelectedFilters>({
     period: [],
     semester: [],
+    block: [],
+    courseLevel: '',
+    mainFieldOfStudy: [],
+    location: '',
+    studyPace: '',
+    examination: '',
     // Initialize other filters as needed
   });
 
@@ -51,7 +65,10 @@ const Dashboard = () => {
     (filterType: keyof SelectedFilters, value: number | string) =>
     (checked: boolean) => {
       setSelectedFilters((prevFilters) => {
-        const currentFilterValues = prevFilters[filterType] || [];
+        const currentFilterValues = prevFilters[filterType] as (
+          | number
+          | string
+        )[];
         if (checked) {
           return {
             ...prevFilters,
@@ -71,17 +88,55 @@ const Dashboard = () => {
       return Object.entries(selectedFilters).every(
         ([filterType, filterValues]) => {
           if (filterValues.length === 0) {
-            return true;
+            return true; // If no filter values are selected, always include the course
           }
-          const courseValue = course[filterType as keyof Course];
-          if (Array.isArray(courseValue)) {
-            // If courseValue is an array, check if it includes any of the filterValues
-            return filterValues.some((filterValue: number) =>
-              courseValue.includes(filterValue)
-            );
+          // Special handling for examination type filters
+          if (filterType === 'examination') {
+            const examinationMapping = {
+              tentamen: 'tenta',
+              laboration: 'lab',
+              projekt: 'projekt',
+              // Note: "övrigt" is handled separately
+            };
+
+            // Filter logic for examination names
+            return course.examination.some((exam: { name: string }) => {
+              const examinationMapping: { [key: string]: string } = {
+                tentamen: 'tenta',
+                laboration: 'lab',
+                projekt: 'projekt',
+                // Note: "övrigt" is handled separately
+              };
+
+              const examNameLower = exam.name.toLowerCase();
+              const hasSpecificType = filterValues.some(
+                (filterValue: string) => {
+                  if (filterValue === 'övrigt') {
+                    // For "Övrigt", check if the examination name does not contain any specific keywords
+                    return !Object.values(examinationMapping).some((keyword) =>
+                      examNameLower.includes(keyword)
+                    );
+                  } else {
+                    // Check if the examination name contains the specific keyword
+                    const keyword = examinationMapping[filterValue];
+                    return keyword ? examNameLower.includes(keyword) : false;
+                  }
+                }
+              );
+              return hasSpecificType;
+            });
           } else {
-            // If courseValue is not an array, check if it equals any of the filterValues
-            return filterValues.includes(courseValue);
+            // Handling for other filter types
+            const courseValue = course[filterType as keyof Course];
+            if (Array.isArray(courseValue)) {
+              // If courseValue is an array, check if it includes any of the filterValues
+              return filterValues.some((filterValue: string | number) =>
+                (courseValue as (number | string)[]).includes(filterValue)
+              );
+            } else {
+              // If courseValue is not an array, check if it matches any of the filterValues
+              return filterValues.includes(courseValue as number | string);
+            }
           }
         }
       );
@@ -104,7 +159,7 @@ const Dashboard = () => {
     <div className="mt-28 sm:mt-40 flex gap-4">
       <Filter handleFilterChange={handleFilterChange} />
 
-      <div className="flex flex-wrap gap-4 justify-between">
+      <div className="flex h-full flex-wrap gap-4 justify-between">
         {filteredCourses.map((course) => (
           <Card key={course.courseCode} className="flex-grow">
             <CardHeader>
@@ -126,7 +181,7 @@ const Dashboard = () => {
               </div>
             </CardContent>
             <CardFooter className="flex gap-4">
-              <p>semester {course.semester.join(', ')}</p>
+              <p>Termin {course.semester.join(', ')}</p>
               <p>Period {course.period.join(', ')}</p>
               <p>Block {course.block.join(', ')}</p>
             </CardFooter>
--- src/pages/Home/Filter.tsx
+++ src/pages/Home/Filter.tsx
@@ -1,3 +1,6 @@
+// react
+import { useState, useEffect } from 'react';
+
 // shadcn
 import {
   Card,
@@ -7,13 +10,26 @@ import {
   CardHeader,
   CardTitle,
 } from '@/components/ui/card';
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from '@/components/ui/accordion';
 import { Checkbox } from '@/components/ui/checkbox';
+
+// data
+import courses from '../../data/courses';
 
 interface SelectedFilters {
   period: number[];
   semester: number[];
-  // Add other filter types as needed, for example:
-  // block: string[];
+  block: number[];
+  courseLevel: string;
+  mainFieldOfStudy: string[];
+  location: string;
+  studyPace: string;
+  examination: string[];
 }
 
 interface FilterProps {
@@ -23,48 +39,255 @@ interface FilterProps {
   ) => (checked: boolean) => void;
 }
 
-const Filter: React.FC<FilterProps> = ({ handleFilterChange }) => (
-  <Card className="sticky max-h-max inset-x-0 top-16 overflow-scroll">
-    {/* Semester */}
-    <CardHeader>
-      <CardTitle>Termin</CardTitle>
-      <CardDescription>Deploy your new project in one-click.</CardDescription>
-    </CardHeader>
+const Filter: React.FC<FilterProps> = ({ handleFilterChange }) => {
+  const [uniqueFields, setUniqueFields] = useState<string[]>([]);
 
-    <CardContent className="flex flex-col gap-4">
-      <div className="items-top flex space-x-2">
-        <Checkbox onCheckedChange={handleFilterChange('semester', 7)} />
-        <div className="grid gap-1.5 leading-none">Termin 7</div>
-      </div>
+  useEffect(() => {
+    const fields = new Set<string>();
+    courses.forEach((course) => {
+      course.mainFieldOfStudy.forEach((field) => {
+        fields.add(field);
+      });
+    });
+    setUniqueFields(Array.from(fields));
+  }, []);
 
-      <div className="items-top flex space-x-2">
-        <Checkbox onCheckedChange={handleFilterChange('semester', 8)} />
-        <div className="grid gap-1.5 leading-none">Termin 8</div>
-      </div>
+  return (
+    <Card
+      className="sticky inset-x-0 top-16 overflow-y-auto h-full"
+      style={{ maxHeight: 'calc(100vh - 74px)' }}
+    >
+      {/* Accordion */}
+      <Accordion
+        type="multiple"
+        defaultValue={[
+          'item1',
+          // 'item2',
+          // 'item3',
+          // 'item4',
+          // 'item5',
+          // 'item6',
+          // 'item7',
+        ]}
+        className="w-full"
+      >
+        <AccordionItem value="item1">
+          <AccordionTrigger>
+            {/* Semester */}
+            <CardHeader>
+              <CardTitle>Termin</CardTitle>
+            </CardHeader>
+          </AccordionTrigger>
+          <AccordionContent>
+            <CardContent className="flex flex-col gap-4">
+              <div className="items-top flex space-x-2">
+                <Checkbox onCheckedChange={handleFilterChange('semester', 7)} />
+                <div className="grid gap-1.5 leading-none">Termin 7</div>
+              </div>
 
-      <div className="items-top flex space-x-2">
-        <Checkbox onCheckedChange={handleFilterChange('semester', 9)} />
-        <div className="grid gap-1.5 leading-none">Termin 9</div>
-      </div>
-    </CardContent>
+              <div className="items-top flex space-x-2">
+                <Checkbox onCheckedChange={handleFilterChange('semester', 8)} />
+                <div className="grid gap-1.5 leading-none">Termin 8</div>
+              </div>
 
-    {/* Period */}
-    <CardHeader>
-      <CardTitle>Period</CardTitle>
-      <CardDescription>Deploy your new project in one-click.</CardDescription>
-    </CardHeader>
-    <CardContent className="flex flex-col gap-4">
-      <div className="items-top flex space-x-2">
-        <Checkbox onCheckedChange={handleFilterChange('period', 1)} />
-        <div className="grid gap-1.5 leading-none">Period 1</div>
-      </div>
+              <div className="items-top flex space-x-2">
+                <Checkbox onCheckedChange={handleFilterChange('semester', 9)} />
+                <div className="grid gap-1.5 leading-none">Termin 9</div>
+              </div>
+            </CardContent>
+          </AccordionContent>
+        </AccordionItem>
 
-      <div className="items-top flex space-x-2">
-        <Checkbox onCheckedChange={handleFilterChange('period', 2)} />
-        <div className="grid gap-1.5 leading-none">Period 2</div>
-      </div>
-    </CardContent>
-  </Card>
-);
+        <AccordionItem value="item2">
+          <AccordionTrigger>
+            {/* Period */}
+            <CardHeader>
+              <CardTitle>Period</CardTitle>
+            </CardHeader>
+          </AccordionTrigger>
+          <AccordionContent>
+            <CardContent className="flex flex-col gap-4">
+              <div className="items-top flex space-x-2">
+                <Checkbox onCheckedChange={handleFilterChange('period', 1)} />
+                <div className="grid gap-1.5 leading-none">Period 1</div>
+              </div>
+
+              <div className="items-top flex space-x-2">
+                <Checkbox onCheckedChange={handleFilterChange('period', 2)} />
+                <div className="grid gap-1.5 leading-none">Period 2</div>
+              </div>
+            </CardContent>
+          </AccordionContent>
+        </AccordionItem>
+
+        <AccordionItem value="item3">
+          <AccordionTrigger>
+            {/* Block */}
+            <CardHeader>
+              <CardTitle>Block</CardTitle>
+            </CardHeader>
+          </AccordionTrigger>
+          <AccordionContent>
+            <CardContent className="flex flex-col gap-4">
+              <div className="items-top flex space-x-2">
+                <Checkbox onCheckedChange={handleFilterChange('block', 1)} />
+                <div className="grid gap-1.5 leading-none">Block 1</div>
+              </div>
+
+              <div className="items-top flex space-x-2">
+                <Checkbox onCheckedChange={handleFilterChange('block', 2)} />
+                <div className="grid gap-1.5 leading-none">Block 2</div>
+              </div>
+
+              <div className="items-top flex space-x-2">
+                <Checkbox onCheckedChange={handleFilterChange('block', 2)} />
+                <div className="grid gap-1.5 leading-none">Block 3</div>
+              </div>
+
+              <div className="items-top flex space-x-2">
+                <Checkbox onCheckedChange={handleFilterChange('block', 2)} />
+                <div className="grid gap-1.5 leading-none">Block 4</div>
+              </div>
+            </CardContent>
+          </AccordionContent>
+        </AccordionItem>
+
+        <AccordionItem value="item4">
+          <AccordionTrigger>
+            {/* Study pace */}
+            <CardHeader>
+              <CardTitle>Studietakt</CardTitle>
+            </CardHeader>
+          </AccordionTrigger>
+          <AccordionContent>
+            <CardContent className="flex flex-col gap-4">
+              <div className="items-top flex space-x-2">
+                <Checkbox
+                  onCheckedChange={handleFilterChange('studyPace', 'Helfart')}
+                />
+                <div className="grid gap-1.5 leading-none">Helfart</div>
+              </div>
+
+              <div className="items-top flex space-x-2">
+                <Checkbox
+                  onCheckedChange={handleFilterChange('studyPace', 'Halvfart')}
+                />
+                <div className="grid gap-1.5 leading-none">Halvfart</div>
+              </div>
+            </CardContent>
+          </AccordionContent>
+        </AccordionItem>
+
+        <AccordionItem value="item5">
+          <AccordionTrigger>
+            {/* Level */}
+            <CardHeader>
+              <CardTitle>Utbildningsnivå</CardTitle>
+            </CardHeader>
+          </AccordionTrigger>
+          <AccordionContent>
+            <CardContent className="flex flex-col gap-4">
+              <div className="items-top flex space-x-2">
+                <Checkbox
+                  onCheckedChange={handleFilterChange(
+                    'courseLevel',
+                    'Grundnivå'
+                  )}
+                />
+                <div className="grid gap-1.5 leading-none">Grundnivå</div>
+              </div>
+
+              <div className="items-top flex space-x-2">
+                <Checkbox
+                  onCheckedChange={handleFilterChange(
+                    'courseLevel',
+                    'Avancerad nivå'
+                  )}
+                />
+                <div className="grid gap-1.5 leading-none">Avancerad nivå</div>
+              </div>
+            </CardContent>
+          </AccordionContent>
+        </AccordionItem>
+
+        <AccordionItem value="item6">
+          <AccordionTrigger>
+            {/* Field of study */}
+            <CardHeader>
+              <CardTitle>Huvudområde</CardTitle>
+            </CardHeader>
+          </AccordionTrigger>
+          <AccordionContent>
+            <CardContent className="flex flex-col gap-4">
+              {uniqueFields.map((field, index) => (
+                <div key={index} className="items-top flex space-x-2">
+                  <Checkbox
+                    onCheckedChange={handleFilterChange(
+                      'mainFieldOfStudy',
+                      field
+                    )}
+                  />
+                  <div className="grid gap-1.5 leading-none">{field}</div>
+                </div>
+              ))}
+            </CardContent>
+          </AccordionContent>
+        </AccordionItem>
+
+        {/* Examination */}
+        <AccordionItem value="itemExaminations">
+          <AccordionTrigger>
+            <CardHeader>
+              <CardTitle>Examination Type</CardTitle>
+            </CardHeader>
+          </AccordionTrigger>
+          <AccordionContent>
+            <CardContent className="flex flex-col gap-4">
+              {['Tentamen', 'Laboration', 'Projekt', 'Övrigt'].map(
+                (type, index) => (
+                  <div key={index} className="items-top flex space-x-2">
+                    <Checkbox
+                      onCheckedChange={handleFilterChange(
+                        'examination',
+                        type.toLowerCase()
+                      )}
+                    />
+                    <div className="grid gap-1.5 leading-none">{type}</div>
+                  </div>
+                )
+              )}
+            </CardContent>
+          </AccordionContent>
+        </AccordionItem>
+
+        <AccordionItem value="item8">
+          <AccordionTrigger>
+            {/* Campus */}
+            <CardHeader>
+              <CardTitle>Campus</CardTitle>
+            </CardHeader>
+          </AccordionTrigger>
+          <AccordionContent>
+            <CardContent className="flex flex-col gap-4">
+              <div className="items-top flex space-x-2">
+                <Checkbox
+                  onCheckedChange={handleFilterChange('location', 'Norrköping')}
+                />
+                <div className="grid gap-1.5 leading-none">Norrköping</div>
+              </div>
+
+              <div className="items-top flex space-x-2">
+                <Checkbox
+                  onCheckedChange={handleFilterChange('location', 'Linköping')}
+                />
+                <div className="grid gap-1.5 leading-none">Linköping</div>
+              </div>
+            </CardContent>
+          </AccordionContent>
+        </AccordionItem>
+      </Accordion>
+    </Card>
+  );
+};
 
 export default Filter;
