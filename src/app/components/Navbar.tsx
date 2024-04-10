0a21cc145a4eb425a3b46f13b9c0414d6c70654e
Filip Malm-Bägén <Filip.bagen@gmail.com>
2024-04-10 14:42:19 +0200

    Refactor Navbar component and add MaxWidthWrapper

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -11,37 +11,46 @@ import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server';
 
 // shadcn
 import { Button } from '@/components/ui/button';
+import { MaxWidthWrapper } from './MaxWidthWrapper';
 
 export async function Navbar() {
   const { isAuthenticated } = getKindeServerSession();
 
   return (
-    <nav className="border-b bg-background h-[10vh] flex items-center">
-      <div className="container flex items-center justify-between">
-        <Link href="/">
-          <h1 className="font-semibold">🧭 Course Finder</h1>
-        </Link>
+    <nav className="flex justify-between items-center sticky h-16 inset-x-0 top-0 z-30 w-full border-b border-gray-200 dark:border-black bg-white/75 dark:bg-gray-950/75 backdrop-blur-lg transition-all">
+      <MaxWidthWrapper>
+        <div className="flex items-center justify-between">
+          <Link href="/">
+            <h4 className="font-semibold">🧭 Course Finder</h4>
+          </Link>
 
-        <div className="flex items-center gap-x-5">
-          <ThemeToggle />
+          <div className="flex items-center gap-x-5">
+            <ThemeToggle />
 
-          {(await isAuthenticated()) ? (
-            <LogoutLink>
-              <Button>Log out</Button>
-            </LogoutLink>
-          ) : (
-            <div className="flex items-center gap-x-5">
-              <LoginLink>
-                <Button>Sign In</Button>
-              </LoginLink>
+            {(await isAuthenticated()) ? (
+              <div className="flex items-center gap-x-5">
+                <Button>
+                  <Link href="/dashboard">Dashboard</Link>
+                </Button>
 
-              <Button variant={'secondary'}>
-                <RegisterLink>Sign up</RegisterLink>
-              </Button>
-            </div>
-          )}
+                <LogoutLink>
+                  <Button variant={'secondary'}>Log out</Button>
+                </LogoutLink>
+              </div>
+            ) : (
+              <div className="flex items-center gap-x-5">
+                <LoginLink>
+                  <Button>Sign In</Button>
+                </LoginLink>
+
+                <Button variant={'secondary'}>
+                  <RegisterLink>Sign up</RegisterLink>
+                </Button>
+              </div>
+            )}
+          </div>
         </div>
-      </div>
+      </MaxWidthWrapper>
     </nav>
   );
 }
