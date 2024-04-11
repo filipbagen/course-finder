84e7643cfc03b3917e13acb25de0ee71f20ecc73
Filip Malm-Bägén <Filip.bagen@gmail.com>
2024-04-11 09:30:42 +0200

    Update database configuration and add user creation logic

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,5 +1,41 @@
 import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server';
 import { redirect } from 'next/navigation';
+import prisma from '../lib/db';
+
+async function getData({
+  email,
+  id,
+  firstName,
+  lastName,
+  profileImage,
+}: {
+  email: string;
+  id: string;
+  firstName: string | undefined | null;
+  lastName: string | undefined | null;
+  profileImage: string | undefined | null;
+}) {
+  const user = await prisma.user.findUnique({
+    where: {
+      id: id,
+    },
+    select: {
+      id: true,
+      stripeCustomerId: true,
+    },
+  });
+
+  if (!user) {
+    const name = `${firstName ?? ''} ${lastName ?? ''}`;
+    await prisma.user.create({
+      data: {
+        id: id,
+        email: email,
+        name: name,
+      },
+    });
+  }
+}
 
 export default async function DashboardLayout({
   children,
@@ -12,6 +48,14 @@ export default async function DashboardLayout({
   if (!user) {
     return redirect('/');
   }
+
+  await getData({
+    email: user.email as string,
+    firstName: user.given_name as string,
+    id: user.id as string,
+    lastName: user.family_name as string,
+    profileImage: user.picture,
+  });
 
   return <main>{children}</main>;
 }
