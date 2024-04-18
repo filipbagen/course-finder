1b2ceb9626b998518702ad59a2ea46e6a8059f51
Filip Malm-Bägén <Filip.bagen@gmail.com>
2024-04-18 15:12:13 +0200

    Add indicator on filter change

--- src/app/dashboard/Filter.tsx
+++ src/app/dashboard/Filter.tsx
@@ -57,55 +57,54 @@ const Filter: React.FC<FilterProps> = ({ onFilterChange, currentFilters }) => {
       style={{ maxHeight: 'calc(100vh - 74px)' }}
     >
       <Accordion type="multiple" defaultValue={['semester']} className="w-full">
-        {accordionItems.map((item) => (
-          <AccordionItemComponent value={item.value} key={item.value}>
-            <AccordionTrigger>
-              <CardHeader>
-                <div className="flex gap-4 items-center">
-                  <item.icon size={20} />
-                  <CardTitle className="mb-0 text-base font-medium">
-                    {item.title}
-                  </CardTitle>
-                </div>
-              </CardHeader>
-            </AccordionTrigger>
-            <AccordionContent>
-              <CardContent className="flex flex-col gap-4">
-                {item.options.map((option) => {
-                  // Convert option to string since FilterState expects string values
-                  const optionString = option.toString();
-                  // Retrieve display value for the current option
-                  const displayValue = item.displayValue(optionString);
-                  // Check if the current option is selected
-                  const isChecked =
-                    currentFilters[item.filterType].includes(optionString);
+        {accordionItems.map((item) => {
+          // Check if any option in this item's filter type is selected
+          const isAnyOptionChecked = currentFilters[item.filterType].length > 0;
 
-                  return (
-                    <CheckboxItem
-                      key={optionString}
-                      filterType={item.filterType}
-                      displayValue={displayValue}
-                      value={optionString}
-                      onChange={(checked) =>
-                        onFilterChange(item.filterType, optionString, checked)
-                      }
-                      checked={isChecked}
-                    />
-                  );
-                })}
-              </CardContent>
-            </AccordionContent>
-          </AccordionItemComponent>
-        ))}
+          return (
+            <AccordionItemComponent value={item.value} key={item.value}>
+              <AccordionTrigger>
+                <CardHeader>
+                  <div className="flex gap-4 items-center">
+                    <item.icon size={20} />
+                    <CardTitle className="mb-0 text-base font-medium">
+                      {item.title}
+                    </CardTitle>
+                    {isAnyOptionChecked && (
+                      <span className="ml-4 h-2 w-2 rounded-full bg-primary"></span>
+                    )}
+                  </div>
+                </CardHeader>
+              </AccordionTrigger>
+              <AccordionContent>
+                <CardContent className="flex flex-col gap-4">
+                  {item.options.map((option) => {
+                    const optionString = option.toString();
+                    const displayValue = item.displayValue(optionString);
+                    const isChecked =
+                      currentFilters[item.filterType].includes(optionString);
+
+                    return (
+                      <CheckboxItem
+                        key={optionString}
+                        filterType={item.filterType}
+                        displayValue={displayValue}
+                        value={optionString}
+                        onChange={(checked) =>
+                          onFilterChange(item.filterType, optionString, checked)
+                        }
+                        checked={isChecked}
+                      />
+                    );
+                  })}
+                </CardContent>
+              </AccordionContent>
+            </AccordionItemComponent>
+          );
+        })}
       </Accordion>
       <CardFooter>
-        <Button
-          onClick={() => {
-            resetFilters();
-          }}
-        >
-          Reset Filter
-        </Button>
+        <Button onClick={resetFilters}>Reset Filter</Button>
       </CardFooter>
     </Card>
   );
