651a2f5e2a4e1aa9d702016c653ce7cd9ba78361
Filip Malm-Bägén <Filip.bagen@gmail.com>
2024-05-18 17:27:14 +0200

    Add Drawer to course dashboard

--- src/app/components/CourseCard.tsx
+++ src/app/components/CourseCard.tsx
@@ -29,14 +29,39 @@ import {
   DropdownMenuTrigger,
 } from '@/components/ui/dropdown-menu';
 import { toast } from 'sonner';
+import { ScrollArea } from '@/components/ui/scroll-area';
+import React, { useMemo, useCallback } from 'react';
+import { Badge } from '@/components/ui/badge';
+import { Separator } from '@/components/ui/separator';
+
+import {
+  Drawer,
+  DrawerClose,
+  DrawerContent,
+  DrawerDescription,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerTitle,
+  DrawerTrigger,
+} from '@/components/ui/drawer';
 
 // icons
-import { MapPin, BookText, SignpostBig, NotebookPen } from 'lucide-react';
+import {
+  MapPin,
+  BookText,
+  SignpostBig,
+  NotebookPen,
+  Star,
+  MessageSquare,
+} from 'lucide-react';
 import { Course, Examination } from '@/app/utilities/types';
 
-export default function CourseCard({ course }: { course: Course }) {
+const CourseCard = ({ course }: { course: Course }) => {
+  // const [isLoading, setIsLoading] = useState(false);
+
   // Function to add course to enrollment
   const addToEnrollment = async (courseId: string, semester: number) => {
+    // setIsLoading(true);
     try {
       const response = await fetch('/api/enrollment', {
         method: 'POST',
@@ -58,6 +83,8 @@ export default function CourseCard({ course }: { course: Course }) {
       });
     } catch (error) {
       console.error(error);
+    } finally {
+      // setIsLoading(false);
     }
   };
 
@@ -80,55 +107,20 @@ export default function CourseCard({ course }: { course: Course }) {
     }
   };
 
-  const handleEnrollment = () => {
+  const handleEnrollment = useCallback(() => {
     const semesters = course.semester;
     if (semesters.length === 1) {
       addToEnrollment(course.id, semesters[0]);
     }
-  };
+  }, [course]);
 
-  return (
-    <Card key={course.id} className="flex-grow h-min">
-      <CardHeader>
-        <div className="flex justify-between">
-          <div>
-            <CardTitle>{course.name}</CardTitle>
-            <CardDescription className="mt-0">{course.code}</CardDescription>
-          </div>
-          <DropdownMenu>
-            <DropdownMenuTrigger asChild>
-              <Button
-                size="icon"
-                aria-label={`Add ${course.name} to your schedule`}
-                onClick={
-                  course.semester.length === 1 ? handleEnrollment : undefined
-                }
-              >
-                +
-              </Button>
-            </DropdownMenuTrigger>
-            {course.semester.length > 1 && (
-              <DropdownMenuContent className="w-56">
-                {course.semester.map((semester) => (
-                  <DropdownMenuItem
-                    key={semester}
-                    onClick={() => addToEnrollment(course.id, semester)}
-                  >
-                    Semester {semester}
-                  </DropdownMenuItem>
-                ))}
-              </DropdownMenuContent>
-            )}
-          </DropdownMenu>
-        </div>
-      </CardHeader>
-
+  const memoizedCourseContent = useMemo(
+    () => (
       <CardContent>
         <div className="flex gap-2 items-center mb-4">
           <MapPin size={16} />
           <p className="[&:not(:first-child)]:mt-0">{course.location}</p>
         </div>
-
         <Accordion type="multiple" className="w-full">
           <AccordionItem value="mainFieldOfStudy" border={false}>
             <div className="flex justify-between">
@@ -145,7 +137,6 @@ export default function CourseCard({ course }: { course: Course }) {
               </CardFooter>
               <AccordionTrigger className="p-0" />
             </div>
-
             <AccordionContent className="flex flex-col gap-4 p-0 mt-6">
               <div>
                 <div className="flex items-center gap-2">
@@ -178,6 +169,114 @@ export default function CourseCard({ course }: { course: Course }) {
           </AccordionItem>
         </Accordion>
       </CardContent>
-    </Card>
+    ),
+    [course]
   );
-}
+
+  return (
+    <Drawer direction="right">
+      <Card key={course.id} className="flex-grow h-min">
+        <DrawerTrigger asChild>
+          <Button>Open Drawer</Button>
+        </DrawerTrigger>
+        <CardHeader>
+          <div className="flex justify-between">
+            <div>
+              <CardTitle>{course.name}</CardTitle>
+              <CardDescription className="mt-0">{course.code}</CardDescription>
+            </div>
+            <DropdownMenu>
+              <DropdownMenuTrigger asChild>
+                <Button
+                  size="icon"
+                  aria-label={`Add ${course.name} to your schedule`}
+                  onClick={
+                    course.semester.length === 1 ? handleEnrollment : undefined
+                  }
+                >
+                  +
+                </Button>
+              </DropdownMenuTrigger>
+              {course.semester.length > 1 && (
+                <DropdownMenuContent className="w-56">
+                  {course.semester.map((semester) => (
+                    <DropdownMenuItem
+                      key={semester}
+                      onClick={() => addToEnrollment(course.id, semester)}
+                    >
+                      Semester {semester}
+                    </DropdownMenuItem>
+                  ))}
+                </DropdownMenuContent>
+              )}
+            </DropdownMenu>
+          </div>
+        </CardHeader>
+
+        {memoizedCourseContent}
+      </Card>
+
+      {/* Drawer */}
+      <DrawerContent
+        showBar={false}
+        className="h-screen top-0 right-0 left-auto mt-0 w-[800px] rounded"
+      >
+        <ScrollArea className="h-screen p-8">
+          <Card key={course.id} className="flex-grow h-min">
+            <CardHeader>
+              <div className="flex justify-between">
+                <div>
+                  <h2>{course.name}</h2>
+                  <CardDescription className="mt-0">
+                    {course.code}
+                  </CardDescription>
+                </div>
+              </div>
+            </CardHeader>
+            <CardContent className="flex flex-col gap-2">
+              <div className="flex gap-2 items-center mb-4">
+                <MapPin size={16} />
+                <p className="[&:not(:first-child)]:mt-0">{course.location}</p>
+              </div>
+
+              <div className="flex gap-4">
+                <div>
+                  <p>Termin {course.semester.join(', ')}</p>
+                </div>
+                <div>
+                  <p>Period {course.period.join(', ')}</p>
+                </div>
+                <div>
+                  <p>Block {course.block.join(', ')}</p>
+                </div>
+              </div>
+              <div className="flex gap-2">
+                {course.mainFieldOfStudy.map((field) => (
+                  <Badge key={field}>{field}</Badge>
+                ))}
+              </div>
+
+              <CardFooter className="flex flex-row gap-4">
+                <div className="flex gap-1 items-center">
+                  <Star size={12} />
+                  {4.2}
+                </div>
+
+                <div className="flex gap-1 items-center">
+                  <MessageSquare size={12} />
+                  {12}
+                </div>
+              </CardFooter>
+            </CardContent>
+          </Card>
+
+          <h5>Rekommenderade förkunskaper</h5>
+          <p>{course.prerequisites}</p>
+          <Separator />
+        </ScrollArea>
+      </DrawerContent>
+    </Drawer>
+  );
+};
+
+export default React.memo(CourseCard);
