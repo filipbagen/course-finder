02d1a5932d30e7be1cf03c41b2d0734a8e76dbc6
Filip Malm-Bägén <Filip.bagen@gmail.com>
2024-05-28 15:24:57 +0200

    Update not-found page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,8 +1,12 @@
 export default function NotFound() {
   return (
-    <div>
-      <h2>Page Not Found</h2>
-      <p>Could not find requested resource</p>
+    <div className="min-h-[70svh] text-center mt-32">
+      <h2>404: Sidan kan inte hittas.</h2>
+      <p>
+        Vi kan inte hitta den sida du söker. Det kan bero på att sidan har
+        flyttats, uppdaterats eller raderats. Använd vår sökfunktion eller gå
+        tillbaka till startsidan för att hitta informationen du behöver.
+      </p>
     </div>
   );
 }
