8d92a3a73acce0e2a4368228c7cf5915c3745ee0
Filip Malm-Bägén <Filip.bagen@gmail.com>
2024-05-30 11:34:05 +0200

    Add reviews section to profile visits

--- src/app/dashboard/social/[id]/page.tsx
+++ src/app/dashboard/social/[id]/page.tsx
@@ -1,12 +1,12 @@
 'use client';
 
+import React, { useEffect, useState } from 'react';
 import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
 import ScheduleView from '@/app/dashboard/schedule/components/ScheduleView';
 import useCourseData from '../../schedule/hooks/useCourseData';
 import useOtherUserData from '../../schedule/hooks/useOtherUserData';
 import Statistics from '../../schedule/Statistics';
 import { Separator } from '@/components/ui/separator';
-import { SkeletonCard } from '@/app/components/SkeletonComponent';
 import {
   Breadcrumb,
   BreadcrumbList,
@@ -15,12 +15,29 @@ import {
   BreadcrumbSeparator,
   BreadcrumbPage,
 } from '@/components/ui/breadcrumb';
+import { Card, CardContent } from '@/components/ui/card';
+import { Star } from 'lucide-react';
 
 type Props = {
   params: {
     id: string;
   };
 };
+
+interface Course {
+  name: string;
+  code: string;
+}
+
+interface Review {
+  id: string;
+  rating: number;
+  comment: string;
+  createdAt: string;
+  updatedAt: string;
+  userId: string;
+  course: Course;
+}
 
 export default function OtherUserSchedule({ params }: Props) {
   const { id } = params;
@@ -31,6 +48,10 @@ export default function OtherUserSchedule({ params }: Props) {
     loading: coursesLoading,
   } = useCourseData(id);
   const { user, loading: userLoading, error: userError } = useOtherUserData(id);
+  const [reviews, setReviews] = useState<Review[]>([]);
+  const [loadingReviews, setLoadingReviews] = useState(true);
+  const [reviewsError, setReviewsError] = useState<string | null>(null);
+
   const defaultUser = user || {
     name: '',
     image: '',
@@ -40,6 +61,25 @@ export default function OtherUserSchedule({ params }: Props) {
     isPublic: true,
     id: '',
   };
+
+  useEffect(() => {
+    const fetchUserReviews = async () => {
+      try {
+        const response = await fetch(`/api/review/userReview?userId=${id}`);
+        if (!response.ok) {
+          throw new Error('Failed to fetch reviews');
+        }
+        const data = (await response.json()) as Review[];
+        setReviews(data);
+      } catch (error) {
+        setReviewsError((error as Error).message);
+      } finally {
+        setLoadingReviews(false);
+      }
+    };
+
+    fetchUserReviews();
+  }, [id]);
 
   return (
     <div className="flex flex-col gap-8">
@@ -71,7 +111,9 @@ export default function OtherUserSchedule({ params }: Props) {
         <Tabs defaultValue="schedule" className="flex flex-col gap-4">
           <TabsList className="flex gap-2 w-min">
             <TabsTrigger value="schedule">Schema</TabsTrigger>
-            <TabsTrigger value="reviews">Recension</TabsTrigger>
+            <TabsTrigger value="reviews">
+              Recension ({reviews.length})
+            </TabsTrigger>
           </TabsList>
 
           <TabsContent value="schedule">
@@ -85,8 +127,40 @@ export default function OtherUserSchedule({ params }: Props) {
 
           <TabsContent value="reviews">
             <div>
-              <h5>Recension</h5>
-              <p>Recensioner kommer snart!</p>
+              {loadingReviews ? (
+                <p>Loading reviews...</p>
+              ) : reviews.length === 0 ? (
+                <p>{user?.name} har inte lämnat någon recension än.</p>
+              ) : reviewsError ? (
+                <p>Error loading reviews: {reviewsError}</p>
+              ) : (
+                reviews.map((review: Review) => (
+                  <div className="flex flex-col gap-4" key={review.id}>
+                    <Card>
+                      <CardContent className="flex flex-col items-start gap-2">
+                        <div className="flex flex-col gap-2">
+                          <div className="flex items-center gap-2">
+                            <h5 className="font-bold">{review.course.name}</h5>{' '}
+                            •<span>{review.course.code}</span>
+                          </div>
+                          <span className="text-muted-foreground">
+                            {new Date(review.createdAt).toLocaleDateString()}
+                          </span>
+                        </div>
+
+                        <div className="flex gap-2 items-center">
+                          <Star size={12} />
+                          {review.rating}
+                        </div>
+
+                        {review.comment ? review.comment : 'No comment'}
+                      </CardContent>
+                    </Card>
+
+                    <Separator />
+                  </div>
+                ))
+              )}
             </div>
           </TabsContent>
         </Tabs>
