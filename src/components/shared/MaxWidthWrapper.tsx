d0941d0057736f5f54af7daa817f6e956cee6970
Filip Malm-Bägén <Filip.bagen@gmail.com>
2025-06-07 16:07:36 +0200

    refactor(MaxWidthWrapper): simplify className and enforce maxWidth style

--- src/components/shared/MaxWidthWrapper.tsx
+++ src/components/shared/MaxWidthWrapper.tsx
@@ -10,10 +10,8 @@ export const MaxWidthWrapper = ({
 }) => {
   return (
     <div
-      className={cn(
-        'mx-auto w-full min-h-screen max-w-screen-xl px-8 md:px-20',
-        className
-      )}
+      className={cn('mx-auto w-full min-h-screen px-8 md:px-20', className)}
+      style={{ maxWidth: '1448px' }}
     >
       {children}
     </div>
