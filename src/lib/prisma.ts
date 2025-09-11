acdb63e026c0bd46c2d28137a870de27f7f9ce1c
Filip Malm-Bägén <Filip.bagen@gmail.com>
2025-09-11 21:00:24 +0200

    style: format prisma.ts for consistency and readability

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,18 +1,21 @@
-import { PrismaClient } from '@prisma/client'
+import { PrismaClient } from '@prisma/client';
 
 const globalForPrisma = globalThis as unknown as {
-  prisma: PrismaClient | undefined
-}
+  prisma: PrismaClient | undefined;
+};
 
 export const prisma =
   globalForPrisma.prisma ??
   new PrismaClient({
-    log: process.env.NODE_ENV === 'development' ? ['query', 'error', 'warn'] : ['error'],
+    log:
+      process.env.NODE_ENV === 'development'
+        ? ['query', 'error', 'warn']
+        : ['error'],
     datasources: {
       db: {
         url: process.env.DATABASE_URL,
       },
     },
-  })
+  });
 
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
